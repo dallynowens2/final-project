9a63be665f6541dba077888858708733a73fdae4
dallynowens2 <dallynowens2@gmail.com>
2021-12-14 13:24:57 -0700

    styled navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav>
-            <NavLink to="/about"> About</NavLink><br/>
-            <NavLink to="/">Home</NavLink>
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar navbar-dark bg-secondary">
+      <div className="container-fluid justify-content-end">
+        <div >
+            <NavLink to="/" className='btn btn-primary m-2'>Home</NavLink>
+            <NavLink to="/about" className='btn btn-primary m-2' > About</NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
